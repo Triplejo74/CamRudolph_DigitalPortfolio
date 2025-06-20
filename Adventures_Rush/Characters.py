ec2615b05e8d8e29eede0c5a68667f9b7dbf7ac6
Triplejo74 <cameronrudolph@live.com>
2025-06-20 17:56:33 -0500

    Added Documentation and Bug fixed

--- Adventures_Rush/Characters.py
+++ Adventures_Rush/Characters.py
@@ -4,60 +4,65 @@ import os
 import pickle as p
 
 class GameCharacter:
-    def __init__(self, name, power, health, loot):
+    #Character Object that has name, power, health, max health, and how much loot they can have
+    def __init__(self, name, power, health, maxHealth, loot):
         self.name = name
         self.power = power
         self.health = health
+        self.maxHealth = maxHealth
         self.loot = loot
 
+    #The fight function takes a random number from the characters power range and subtracts it from the other
     def fight(self, other):
         atk = random.randrange(0, self.power)
         other.health -= atk
         print(f"{self.name} attacks {other.name} for {atk} HP.")
 
-    def save_health(self, filename):
-        with open(filename, "wb") as hFile:
-            p.dump(self.health, hFile)
+    # Initialize player and monsters
+player = GameCharacter("Adventurer", 20, 80, 80, 0)
 
-    def load_distance(self, filename):
-        try:
-            save_dir = os.path.join("Adventures_Rush", "Saved_Game")
-            with open(os.path.join(save_dir, "distance.pk1"), "rb") as dFile:
-                self.distance = p.load(dFile)
-        except FileNotFoundError:
-            print("No saved distance file found. Using default distance.")
-
-    def load_MonsterSlain(self, filename):
-        try:
-            save_dir = os.path.join("Adventures_Rush", "Saved_Game")
-            with open(os.path.join(save_dir, "MonsterSlain.pk1"), "rb") as mFile:
-                self.MonsterSlain = p.load(mFile)
-        except FileNotFoundError:
-            print("No saved monster slain file found. Using default monster slain.")
-
-    def load_inventory(self, filename):
-        try:
-            save_dir = os.path.join("Adventures_Rush", "Saved_Game")
-            with open(os.path.join(save_dir, "inventory.pk1"), "rb") as iFile:
-                self.inventory = p.load(iFile)
-        except FileNotFoundError:
-            print("No saved inventory file found. Using default inventory.")
-
-    def load_health(self, filename):
-        try:
-            save_dir = os.path.join("Adventures_Rush", "Saved_Game")
-            with open(os.path.join(save_dir, "health.pk1"), "rb") as hFile:
-                self.health = p.load(hFile)
-        except FileNotFoundError:
-            print("No saved health file found. Using default health.")
-
-# Initialize player and monsters
-player = GameCharacter("Adventurer", 20, 80, 0)
-
-
-monster1 = GameCharacter("Goblin", 10, 20, 3)
-monster2 = GameCharacter("Orc", 17, 30, 5)
-monster3 = GameCharacter("Giant Spider", 12, 25, 4)
-monster4 = GameCharacter("Slime", 5, 40, 3)
-monster5 = GameCharacter("Dragon", 20, 35, 8)
+monster1 = GameCharacter("Goblin", 10, 20, 20, 3)
+monster2 = GameCharacter("Orc", 17, 30, 30, 5)
+monster3 = GameCharacter("Giant Spider", 12, 25, 25, 4)
+monster4 = GameCharacter("Slime", 5, 40, 40, 3)
+monster5 = GameCharacter("Dragon", 20, 35, 35, 8)
 monsterList = [monster1, monster2, monster3, monster4, monster5]
+
+    #Player character progress saving. In the works pending research
+"""
+def save_health(self, filename):
+    with open(filename, "wb") as hFile:
+        p.dump(self.health, hFile)
+
+def load_distance(self, filename):
+    try:
+        save_dir = os.path.join("Adventures_Rush", "Saved_Game")
+        with open(os.path.join(save_dir, "distance.pk1"), "rb") as dFile:
+            self.distance = p.load(dFile)
+    except FileNotFoundError:
+        print("No saved distance file found. Using default distance.")
+
+def load_MonsterSlain(self, filename):
+    try:
+        save_dir = os.path.join("Adventures_Rush", "Saved_Game")
+        with open(os.path.join(save_dir, "MonsterSlain.pk1"), "rb") as mFile:
+            self.MonsterSlain = p.load(mFile)
+    except FileNotFoundError:
+        print("No saved monster slain file found. Using default monster slain.")
+
+def load_inventory(self, filename):
+    try:
+        save_dir = os.path.join("Adventures_Rush", "Saved_Game")
+        with open(os.path.join(save_dir, "inventory.pk1"), "rb") as iFile:
+            self.inventory = p.load(iFile)
+    except FileNotFoundError:
+        print("No saved inventory file found. Using default inventory.")
+
+def load_health(self, filename):
+    try:
+        save_dir = os.path.join("Adventures_Rush", "Saved_Game")
+        with open(os.path.join(save_dir, "health.pk1"), "rb") as hFile:
+            self.health = p.load(hFile)
+    except FileNotFoundError:
+        print("No saved health file found. Using default health.")
+        """
--- Adventures_Rush/Main.py
+++ Adventures_Rush/Main.py
@@ -23,20 +23,21 @@ while coin == 0:
 
 Do you wish to play Adventurer's Rush?
 y = Yes
-n = No
-o = Open saved file \n""")
-    
-# Think moving load game after this section so that way it would load after the game starts for less confusion
+n = No \n""")
 
     if ply == "y":
         while town == 0: # The user is given the option to enter the town to upgrade equipment, buy potions, or sell monster parts
             dis = 100
+
+            #The first prompt of for the user of where to go
             peanut = input("""Your Adventure awaits!
 What will you do?
 t = Go into town
 c = Go into the Caves \n""")
             if peanut == "t":
                 while tVis == 0:
+
+                    #Town prompt giving the player options to better their hero 
                     shop = input("""You enter the town and notice a few shops. Where do you go?
 b = Blacksmith (Upgrade armor)
 t = Trainer (Train with a trainer)
@@ -45,16 +46,21 @@ m = Monster Guild (Sell your monster parts)
 i = Inventory (Check your inventory)
 l = Leave (Go into the cave)
 q = Quit (Quit the Game)\n""")
+                    
+                    #Blacksmith adds more HP to the player's total health for the price of 50 gold
                     if shop == "b":
                         bAns = input("Would you like to upgrade your armor for 50 gold giving you +5 HP? (y = Yes, n = No)\n")
                         if bAns == "y":
                             if inventory["Gold"] - 50 >= 0:
                                 inventory["Gold"] -= 50
                                 hUP += 5
+                                c.player.maxHealth = c.player.health + hUP
                                 print("Remaining Gold: ", inventory["Gold"])
                                 print("New HP: ", c.player.health + hUP, '/', 80 + hUP)
                             else:
                                 input("You do not have enough gold.\nPress \"Enter\" to continue")
+
+                    #Trainer adds two more damage automatically before the damaged is rolled. Training is 30 gold
                     if shop == "t":
                         tAns = input("Would you like to train with a master to improve your damage by 2 for 30 Gold? (y = Yes, n = No)\n")
                         if tAns == "y":
@@ -65,6 +71,8 @@ q = Quit (Quit the Game)\n""")
                                 print("New Damage: ", c.player.power + dUP)
                             else:
                                 input("You do not have enough gold.\nPress \"Enter\" to continue")
+
+                    #Alchemist adds another potion to the inventory for the price of 20 gold
                     if shop == "a":
                         aAns = input("Would you like to go into the Alchemist's shop and buy a potion for 20 gold? (y = Yes, n = No)\n")
                         if aAns == "y":
@@ -75,6 +83,8 @@ q = Quit (Quit the Game)\n""")
                                 print("Potions: ", inventory["Potions"])
                             else:
                                 input("You do not have enough gold.\nPress \"Enter\" to continue")
+
+                    #Moster guild allows the player to sell the Monster parts they gathered in the cave for 10 gold a piece
                     if shop == "m":
                         mAns = input("Would you like to sell your monster parts for 10 Gold a piece? (y = Yes, n = No)\n")
                         if mAns == "y":
@@ -85,6 +95,8 @@ q = Quit (Quit the Game)\n""")
                                 print("Total Gold: ", inventory["Gold"])
                             else:
                                 input("You do not have any Monster Parts to sell.\nPress \"Enter\" to continue")
+
+                    #Inventory displays the players gold, potions, and monster parts. It also displays the player's health and damage range
                     if shop == "i":
                         print(inventory)
                         print("Current HP: ", c.player.health + hUP)
@@ -97,51 +109,72 @@ q = Quit (Quit the Game)\n""")
                             break
                         else:
                             input("You decide to stay in town.\nPress \"Enter\" to continue")
+
+                    #Quit quits and closes out the game
                     if shop == "q":
-                        print("The Adventurer left the game.")
+                        input("The Adventurer left the game. Press Space to close")
                         quit()
+
+            #Puts the player into the caves using random number chances to encounter an enemy
             if peanut == "c":
+                mSlain = 0
                 while dis >= 1:
                     encounterChance = random.randrange(10)
-                    if encounterChance < 4: 
+                    if encounterChance <= 3: 
                         opponent = random.choice(c.monsterList)
-                        print("\nA " + opponent.name + " jumps out and attacks.")
-                        while c.player.health > 0 and opponent.health > 0:
-                            c.player.fight(opponent)
+                        print("\nA " + opponent.name + " appears and attacks.", encounterChance) 
+                        while c.player.health > 0 and opponent.health > 0:      #The battle phase using each characters HP and damage to attack hurt each other     
+                            c.player.fight(opponent)                            #the Fight function has a random number pulled from the characters damage range
                             opponent.fight(c.player)
                             print("Current health: " + str(c.player.health + hUP))
                             print("Opponents health: " + str(opponent.health - dUP) + "\n")
                             btl = input("What will you do? (f = Fight, r = Runaway) ")
+
+                             #Player could also run setting them back by 15
                             if btl == "r":
                                 print("You run away but your progress to leave the cave is set back.")
                                 dis += 15
                                 rPau = input("Press \"Enter\" to continue \n")
                                 break
+
+                            #Once the opponent is defeated, the player is given monster parts based on the opponent they fought
                             if opponent.health - dUP <= 0:
                                 print("You slayed the " + opponent.name)
                                 mSlain += 1
-                                mPrt = random.randrange(0, 8)
-                                print("You loot " + str(mPrt) + " parts from the " + opponent.name + " and put them in your inventory")
+                                mPrt = random.randrange(opponent.loot)
+                                print("You loot " + str(mPrt) + " parts from the " + opponent.name + " and put them in your inventory.")
                                 inventory["Monster Parts"] += mPrt
                                 kPau = input("Press \"Enter\" to continue \n")
+                                opponent.health 
+                                opponent.health = opponent.maxHealth
                                 break
+
+                        #If the player's HP goes to or below zero the game ends
                         if c.player.health + hUP <= 0:
-                            print("You were slain by a monster and defeated " + str(mSlain) + " monsters.")
+                            input("You were slain by a monster and defeated " + str(mSlain) + " monsters. Press \"Enter\" to quit.")
                             quit()
 
+                    #The player's movement is tracked and is prompted on what to do
                     print("Meters from escaping the cave " + str(dis))
                     dis -= random.randrange(1, 20)
-                    Ans = input("What will you do?(c = Continue, g = Give up, h = Heal, i = Inventory, s = Save game) \n")
+                    Ans = input("What will you do?(c = Continue, g = Give up, h = Heal, i = Inventory) \n")
+
+                    #During the cave the player can quit out inbetween movement and battling
                     if Ans == "g":
-                        print("The Adventurer gave up and was lost to the cave.")
-                        quit()
+                        giveUp = input("Are you sure you want to give up? (\"y\" = yes or \"n\" = no)\n")
+                        if giveUp == "y":
+                            input("The Adventurer gave up and was lost to the cave.")
+                            quit()
+                        input("Continuing on. (Press \"Enter\" to continue)")
+
+                    #The player can choose to heal while in the cave
                     if Ans == "h":
                         while hPause == 0:
                             hAns = input("Would you like to use a potion? (y = Yes, n = No) \n")
                             if hAns == "y":
-                                if inventory["Potions"] > 0:
-                                    inventory["Potions"] -= 1
-                                    if c.player.health <= 79 + hUP:
+                                if inventory["Potions"] > 0:            #The player is prompted to use a potion, If they do it will
+                                    inventory["Potions"] -= 1           #subtract one from their total potion pool and add 25 HP back.
+                                    if c.player.health <= 79 + hUP:     #If the player is at or above max health it will have no effect
                                         c.player.health += 25
                                         print("Number of potions left: ", inventory["Potions"])
                                         print("Current HP: ", c.player.health + hUP, "/", 80 + hUP)
@@ -150,15 +183,19 @@ q = Quit (Quit the Game)\n""")
                                         hMax = input("Press \"Enter\" to continue \n")
                                         break
                                 else:
-                                    print("You look through bag and find no potions to use")
+                                    print("You look through bag and find no potions to use") 
                                     nPot = input("Press \"Enter\" to continue \n")
                                     break
                             else:
                                 break
+
+                    #The player checks their inventory and displays their current health too 
                     if Ans == "i":
-                        print(inventory)
+                        print(inventory, "\nCurrent HP: ", c.player.health + hUP)
                         iPau = input("Press \"Enter\" to continue\n")
-                    if Ans == "s":
+
+                    #Save game function that is not quite working as intended. Taken out until fixed
+                    '''if Ans == "s":
                         save_dir = os.path.join("Adventures_Rush", "Saved_Game")
                         if not os.path.exists(save_dir):
                             os.makedirs(save_dir)
@@ -175,12 +212,15 @@ q = Quit (Quit the Game)\n""")
                             print("The Adventurer left the game.")
                             quit()
                         else:
-                            print("You continue your adventure.")
+                            print("You continue your adventure.")'''
 
-
-                print("You've left the caves after defeating ", mSlain, " monsters!")
+                #After successfully completing a run the monsters they slain will display and the player goes back to full HP
+                print("You've left the caves after defeating ", mSlain, " monsters and rest in town!")
+                c.player.health = c.player.maxHealth
         town = 1
-    if ply == "o":
+
+        #Load Game function that is not working. Doing more research into progress saving
+    '''if ply == "o":
         try:
             save_dir = os.path.join("Adventures_Rush", "Saved_Game")
             c.player.load_distance(os.path.join(save_dir, "distance.pk1"))
@@ -195,11 +235,9 @@ q = Quit (Quit the Game)\n""")
             else:
                 print("The Adventurer left the game.")
                 quit()
-            
-        
         except FileNotFoundError:
-            print("No saved game file found.")
+            print("No saved game file found.")'''
 
-        
+    #If the player chooses to not play it will quit the program. 
     if ply == "n":
         quit()
